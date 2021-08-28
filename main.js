96ce4ff0cd73eb840d1c7760d1ce130d9245754a
David Tang <dtang859@gmail.com>
2021-08-28 16:05:59 -0700

    2. Fetch and render a random activity from the Bored API

--- main.js
+++ main.js
@@ -1,15 +1,9 @@
-const renderPerson = Handlebars.compile($("#person-template").html());
+const renderActivity = Handlebars.compile($("#activity-template").html());
 
-const david = {
-  name: "David Tang",
-  // name: "David Tang<script>alert('hi')</script>",
-  image:
-    "https://www.gravatar.com/avatar/1c31a96c0a474a6795637081bde42acc.png?s=500",
-  hobbies: [
-    { name: "Credit Card Rewards", years: 2 },
-    { name: "Ice Hockey", years: 25 },
-  ],
-};
-
-const html = renderPerson(david);
-$("#results").html(html);
+$.ajax({
+  type: "GET",
+  url: "https://www.boredapi.com/api/activity",
+}).then((activity) => {
+  const html = renderActivity(activity);
+  $("#results").html(html);
+});
