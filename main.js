fced208907b2bf7a214cf71f657d496c613a35bd
David Tang <dtang859@gmail.com>
2021-08-28 16:12:48 -0700

    3. Event Delegation

--- main.js
+++ main.js
@@ -1,9 +1,16 @@
 const renderActivity = Handlebars.compile($("#activity-template").html());
 
+let lastActivityType;
+
 $.ajax({
   type: "GET",
   url: "https://www.boredapi.com/api/activity",
 }).then((activity) => {
+  lastActivityType = activity.type;
   const html = renderActivity(activity);
   $("#results").html(html);
+});
+
+$("#results").on("click", "button", function () {
+  console.log(lastActivityType);
 });
